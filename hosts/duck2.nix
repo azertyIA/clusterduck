bcb5da49f3f8b8de21c91b111086b9ed4297179a
azertyIA <twinarmageddons11011@gmail.com>
2026-06-21 00:08:02 -0400

    Fixed Minor Stylistic Choice

--- hosts/duck2.nix
+++ hosts/duck2.nix
@@ -6,7 +6,7 @@
       ./duck2-hardware-configuration.nix
     ];
 
-  networking.hostName = "Pintail";
+  networking.hostName = "pintail";
   networking.interfaces.eno1.ipv4.addresses = [{
     address = "10.42.0.12";
     prefixLength = 24;
